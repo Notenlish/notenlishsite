3c3af1ade30badce5e541fd215d304c01d4c3763
Notenlish <71970100+Notenlish@users.noreply.github.com>
2025-07-20 17:50:19 +0300

    Update route.tsx

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -7,7 +7,7 @@ export function GET(request: Request) {
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white text-black">
+      <div tw="flex flex-col w-full h-full items-center justify-center bg-gradient-to-br from-white to-neutral-200 text-black">
         <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
           <h2 tw="flex flex-col text-4xl font-bold text-left">{title}</h2>
         </div>
