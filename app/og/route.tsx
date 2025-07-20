529b0a37b6d2952f83a9e8614c8738c1510235f6
Notenlish <71970100+Notenlish@users.noreply.github.com>
2025-07-20 17:45:22 +0300

    Update route.tsx

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -7,9 +7,9 @@ export function GET(request: Request) {
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center text-black dark:text-white bg-gradient-to-br from-white to-neutral-200 dark:from-neutral-900 dark:to-neutral-800" style={{ backgroundImage: "linear-gradient(to bottom right, oklch(100% 0 0), oklch(83% 0 0))" }}>
+      <div tw="flex flex-col w-full h-full items-center justify-center bg-white text-black">
         <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold text-left text-black dark:text-white">{title}</h2>
+          <h2 tw="flex flex-col text-4xl font-bold text-left">{title}</h2>
         </div>
       </div>
     ),
