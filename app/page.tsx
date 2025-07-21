43e448fab68b87449ea0924fb38e15ecf97263be
Notenlish <71970100+Notenlish@users.noreply.github.com>
2025-07-22 01:02:17 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,9 +23,7 @@ export default function Page() {
           experience with web development.
         </p>
         <p>
-          I use Python and TypeScript generally. Most of my experience is with:
-          Next.js, React, and Tailwind CSS. I also have experience with Django,
-          and Flask.
+          I use Python and TypeScript generally. I've mostly dabbled with Next.js, React, and Tailwind CSS. I also have experience with Django and Flask.
         </p>
         <p>You can find me on <a href={socialLinks.twitter} target="_blank">Twitter</a> and <a href={socialLinks.github} target="_blank">GitHub</a>. Contact me via the <a href="/contact">Contact</a> page.</p>
         <p>I also have a <a href="https://notenlish.itch.io" target="_blank">itch.io</a> page.</p>
